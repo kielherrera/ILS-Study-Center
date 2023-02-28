d40d7d60ee256b956c678b7fdee25d3537e99816
falarion08 <barrion.ryanonil@gmail.com>
2023-02-28 17:29:01 +0800

    Designed log in page
    >Installed body parser, and ejs
    > handled login page using render
    >created views folder for html files
    >created public folder for css/javascript, and image files

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-const http = require('http');
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+
+
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
 
 app.get('/', function(req,res){
-    res.send('Hello World');
-})
+    res.render('login_page');
+});
+
+app.post('/', function(req,res){
+
+});
 
 var server = app.listen(3000, function() {
     console.log("Server is running at port 3000...");
