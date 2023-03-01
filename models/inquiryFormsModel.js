957980f9aa34eee22450650c6b9bf5221785bf6f
davideeeee2001 <74911350+davideeeee2001@users.noreply.github.com>
2023-03-01 23:49:26 +0800

    created connection to db

---
+++ models/inquiryFormsModel.js
@@ -0,0 +1,49 @@
+var mongoose = require('mongoose');
+
+var inquiryForm = new mongoose.Schema({
+
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  fbLink: {
+    type: String,
+    required: true,
+  },
+  childName: {
+    type: String,
+    required: true,
+  },
+  birthDate: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+    unique: true,
+    enum: ['Male', 'Female']
+  },
+  program: {
+    type: String,
+    required: true,
+    enum: ['Tutoring', 'Playdate']
+  },
+  inquiry: {
+    type: String,
+    required: true,
+  },
+  
+});
+
+module.exports = mongoose.model('inquiryForms', inquiryForm);
--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
+const db = require('./models/db.js');
+const adminAccounts = require('./models/adminAccountsModel.js');
+const inquiryForms = require('./models/inquiryFormsModel.js');
 const app = express();
 
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
+
+db.connect();
+
+app.use(session({
+    'secret': 'ils-session',
+    'resave': false,
+    'saveUninitialized': false,
+    store: MongoStore.create({mongoUrl: 'mongodb://localhost:27017/ILS-Study-Center'})
+}));
 
 app.get('/', function(req,res){
     res.render('login_page');
@@ -20,7 +35,13 @@ app.post('/', function(req,res){
 });
 
 app.post('/inquire', function(req,res){
-    res.redirect('/');
+    db.insertOne(inquiryForms, {name: req.body.inquirer_name, phoneNumber: req.body.inquirer_mobile_number, 
+                email: req.body.inquirer_email_address, fbLink: req.body.inquirer_facebook_link, 
+                childName: req.body.child_name, birthDate: req.body.child_birthdate, gender: req.body.gender, 
+                program: req.body.program, inquiry: req.body.inquiry}, (result) => {
+
+        res.redirect('/');
+    });
 })
 
 
